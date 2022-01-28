f2f582eda9d97e0c5ba95cef0768d0a078db86a7
Cezary Grzanka <cezarygrzanka@gmail.com>
2022-01-28 17:03:16 +0100

    Fix declare typing

--- src/types/module.d.ts
+++ src/types/module.d.ts
@@ -1,1 +1,9 @@
-declare module 'calendar.ts' {}
+import { MonthArray, MonthCalendar } from './index';
+
+declare module '@cgsh/calendarts' {
+  export function getMonth(month: number, year?: number): MonthCalendar;
+  export function getMonthArray(month: MonthCalendar): MonthArray;
+
+  export type TMonthCalendar = MonthCalendar;
+  export type TMonthArray = MonthArray;
+}
